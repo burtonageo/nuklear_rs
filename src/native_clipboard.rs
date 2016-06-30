c64a1eb9eb2e0b48694425501951c8e5d986a72f
George Burton <burtonageo@gmail.com>
2016-06-30 09:12:26 +0100

    Flesh out the WindowsClipboard a bit more, and add a test for the native clipboard

--- src/native_clipboard.rs
+++ src/native_clipboard.rs
@@ -25,7 +25,15 @@ mod mac_clipboard {
 mod windows_clipboard {
     use winapi;
 
-    pub struct WindowsClipboard;
+    pub struct WindowsClipboard {
+        _priv: ()
+    }
+
+    impl Default for WindowsClipboard {
+        fn default() -> Self {
+            unimplemented!();
+        }
+    }
 
     impl ::Clipboard for WindowsClipboard {
         fn copy(&mut self, text: &str) {
@@ -36,8 +44,6 @@ mod windows_clipboard {
             unimplemented!();
         }
     }
-
-    pub type NativeClipboard = WindowsClipboard;
 }
 
 #[cfg(target_os = "macos")]
@@ -45,3 +51,16 @@ pub type NativeClipboard = mac_clipboard::CocoaClipboard;
 
 #[cfg(target_os = "windows")]
 pub type NativeClipboard = windows_clipboard::WindowsClipboard;
+
+#[cfg(all(test, any(target_os = "macos", target_os = "windows")))]
+mod native_clipboard_tests {
+    use super::NativeClipboard;
+    use ::Clipboard;
+    #[test]
+    fn test_native_clipboard() {
+        const TEST_TEXT: &'static str = "BoomShakalaka";
+        let clipboard = NativeClipboard::default();
+        clipboard.copy(TEST_TEXT);
+        assert_eq!(clipboard.get_paste_text(), TEST_TEXT);
+    }
+}
