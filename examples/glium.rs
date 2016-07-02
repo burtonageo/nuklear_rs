67cd5a04993a46cb4297433425283521746475d9
George Burton <burtonageo@gmail.com>
2016-07-02 11:35:27 +0100

    Use an nk::Color in the example to check that linking works

--- examples/glium.rs
+++ examples/glium.rs
@@ -2,6 +2,7 @@ extern crate glium;
 extern crate nuklear_rs as nk;
 
 use glium::{DisplayBuild, Surface};
+use nk::Color;
 
 fn main() {
     let display = glium::glutin::WindowBuilder::new()
@@ -9,6 +10,8 @@ fn main() {
         .with_title(format!("Nuklear Widgets Example"))
         .build_glium()
         .unwrap();
+
+    let bg_color = Color::rgb(0, 0, 255);
 
     loop {
         for e in display.poll_events() {
@@ -19,7 +22,8 @@ fn main() {
         }
 
         let mut target = display.draw();
-        target.clear_color(0.0, 0.0, 1.0, 1.0);
+        let col_slice: [f32; 4] = bg_color.into();
+        target.clear_color(col_slice[0], col_slice[1], col_slice[2], col_slice[3]);
         target.finish().unwrap();
     }
 }
