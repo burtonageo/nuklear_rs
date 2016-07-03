38e026d141b04b324a3f10ba7bcd7eb4f6f2baae
George Burton <burtonageo@gmail.com>
2016-07-03 12:23:54 +0100

    Add dependency on user32 lib for Windows

--- src/lib.rs
+++ src/lib.rs
@@ -21,6 +21,9 @@ extern crate objc;
 #[cfg(all(target_os = "windows", feature = "native_clipboard"))]
 extern crate winapi;
 
+#[cfg(all(target_os = "windows", feature = "native_clipboard"))]
+extern crate user32;
+
 #[cfg(feature = "rust_allocator")]
 mod rust_allocator;
 
